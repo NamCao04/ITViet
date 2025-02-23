aeb95206f30d5ba63ac440c6bbe10eb1c2f371d2
0306221306namcao <0306221306@caothang.edu.vn>
2025-02-23 16:57:51 +0700

    done 100%

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -309,7 +309,7 @@ const Home = () => {
                     </div>
 
                     <div className="header-images">
-                        <img src="https://itviec.com/rails/active_storage/representations/proxy/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBeWtVVEE9PSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--1a31ceb56a18eaff830ef23798c9e1f3fe2e0613/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaDdCem9MWm05eWJXRjBPZ2wzWldKd09oSnlaWE5wZW1WZmRHOWZabWwwV3dkcEFsZ0NNQT09IiwiZXhwIjpudWxsLCJwdXIiOiJ2YXJpYXRpb24ifX0=--c7ff83b896d4db7bd4f14a67b569154fd1b65a60/LinkedIn%20Business%20Banner%20June%202024%20Update%20(600x400px)%20.png" height="37" alt="Logo" className="img-size1" />
+                        <img src="https://itviec.com/rails/active_storage/representations/proxy/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBeVNPVmc9PSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--399f7d7d7b0977c73a8c930a6d2d00724027d37b/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaDdCem9MWm05eWJXRjBPZ2wzWldKd09oSnlaWE5wZW1WZmRHOWZabWwwV3dkcEFsZ0NNQT09IiwiZXhwIjpudWxsLCJwdXIiOiJ2YXJpYXRpb24ifX0=--c7ff83b896d4db7bd4f14a67b569154fd1b65a60/1000013345.jpg" height="37" alt="Logo" className="img-size1" />
                         <img src="https://itviec.com/rails/active_storage/representations/proxy/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBMmZvSXc9PSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--88c82102b4c6782414b90e4077f75449b9789560/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaDdCem9MWm05eWJXRjBPZ2wzWldKd09oSnlaWE5wZW1WZmRHOWZabWwwV3dkcEFhb3ciLCJleHAiOm51bGwsInB1ciI6InZhcmlhdGlvbiJ9fQ==--bb0ebae071595ab1791dc0ad640ef70a76504047/Logo%20MB%20he%20mau%20RGB%2001.png" height="37" alt="Logo" className="img-size2" />
                         <div className="header-conten">
                             <h3 className="header-main">NAB Innovation Centre Vietnam</h3>
